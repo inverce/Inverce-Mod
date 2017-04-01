3029c4eab863fb90b47c8b422c6c6b437c46f3a3
Inverce <dominik.opolski@gmail.com>
2017-04-01 12:50:39 +0200

    traverse tree collection

--- Core/src/main/java/com/inverce/mod/core/collections/TraverseTreeCollection.java
+++ Core/src/main/java/com/inverce/mod/core/collections/TraverseTreeCollection.java
@@ -5,8 +5,12 @@ import android.support.annotation.NonNull;
 import java.util.AbstractCollection;
 import java.util.AbstractList;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Queue;
 import java.util.Stack;
 
 import static com.inverce.mod.core.collections.TraversalMethod.ASC;
@@ -15,7 +19,7 @@ import static com.inverce.mod.core.collections.TraversalMethod.DESC;
 import static com.inverce.mod.core.collections.TraversalMethod.DFS;
 
 public class TraverseTreeCollection<T extends TreeNode<T>> extends AbstractCollection<T> {
-    private final int flags;
+    private final boolean isDfs, isAsc;
     private final T node;
 
     public TraverseTreeCollection(T node) {
@@ -26,10 +30,10 @@ public class TraverseTreeCollection<T extends TreeNode<T>> extends AbstractColle
         if ((flags & 1) != 1) {
             throw new IllegalStateException("Specify method. DFS or BFS");
         }
-        if ((flags & DFS & BFS)== (DFS & BFS)) {
+        if ((flags & (DFS | BFS))== (DFS | BFS)) {
             throw new IllegalStateException("Only one of DFS or BFS is allowed");
         }
-        if ((flags & ASC & DESC)== (ASC & DESC)) {
+        if ((flags & (ASC | DESC))== (ASC | DESC)) {
             throw new IllegalStateException("Only one of ASC or DESC is allowed");
         }
         if (node == null) {
@@ -37,7 +41,8 @@ public class TraverseTreeCollection<T extends TreeNode<T>> extends AbstractColle
         }
 
         this.node = node;
-        this.flags = flags;
+        this.isDfs = (flags & DFS) == DFS;
+        this.isAsc = (flags & ASC) == ASC;
     }
 
     @NonNull
@@ -52,32 +57,55 @@ public class TraverseTreeCollection<T extends TreeNode<T>> extends AbstractColle
     }
 
     private class Iter implements Iterator<T> {
-        int inSize = size(), currentItem;
-        Stack<Integer> stack;
-        Stack<T> nodes;
+        Stack<T> box;
+        Queue<T> que;
 
         Iter() {
-            this.stack = new Stack<>();
-            this.nodes = new Stack<>();
-            this.stack.push(0);
-            this.nodes.push(node);
+            this.box = new Stack<>();
+            this.que = new LinkedList<>();
+            if (isDfs) {
+                this.box.push(node);
+            } else {
+                pushChildren(que, node);
+            }
         }
 
         @Override
         public boolean hasNext() {
-            return inSize != currentItem;
+            return isDfs ? !box.empty() : !que.isEmpty();
         }
 
         @Override
         public T next() {
-            return null;
+            if (!hasNext ( )) {
+                throw new NoSuchElementException("tree ran out of elements");
+            }
+            T node;
+
+            if (isDfs) {
+                node = box.pop();
+                pushChildren(box, node);
+            } else {
+                node = que.remove();
+                pushChildren(que, node);
+            }
+            return node;
         }
+
+        private void pushChildren(Collection<T> into, T node) {
+            if (isAsc) {
+                into.addAll(new ReverseOrderList<>(node.getChildren()));
+            } else {
+                into.addAll(node.getChildren());
+            }
+        }
+
     }
 
-    static class ReverseOrderList<E> extends AbstractList<E> {
+    private static class ReverseOrderList<E> extends AbstractList<E> {
         private List<E> inner;
 
-        public ReverseOrderList(List<E> inner) {
+        ReverseOrderList(List<E> inner) {
             if (inner == null) {
                 inner = new ArrayList<>();
             }
--- test/src/main/java/com/example/test/MainActivity.java
+++ test/src/main/java/com/example/test/MainActivity.java
@@ -7,41 +7,26 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.inverce.mod.core.IM;
 import com.inverce.mod.core.Log;
-import com.inverce.mod.core.collections.TreeNode;
-import com.inverce.mod.events.Event;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        IMInitializer.initialize(this);
         setContentView(R.layout.activity_main);
 
-        List<IntNode> all = new ArrayList<>();
-        IntNode root = createTree(new IntNode(1), all, 2, 5);
-
         final Handler handler = new Handler(Looper.getMainLooper());
-
-        Event<Intre> test = new Event<>(Intre.class);
-
-        test.addListener(new IntreImpl());
-        test.addListener(() -> Log.w("pp2"));
-
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 Log.w("d");
                 for (int i=0;i<1; i++) {
                     IM.onBg().schedule(() -> {
-                        test.post().pp();
-                        System.gc();
+
+
                     }, 1500, TimeUnit.MILLISECONDS);
                 }
                 handler.postDelayed(this, 2000);
@@ -50,39 +35,4 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private IntNode createTree(IntNode node, List<IntNode> all, int level, int size) {
-        for (int i = 0; i < size; i++) {
-            IntNode inNode = new IntNode(node.value * 10 + i);
-            all.add(inNode);
-            if (level > 0) {
-                createTree(inNode, all, level - 1, size);
-            }
-            node.getChildren().add(inNode);
-        }
-        return node;
-    }
-
-    static class IntreImpl implements Intre {
-        @Override
-        public void pp() {
-            Log.w("PP1");
-        }
-    }
-
-    class IntNode extends TreeNode<IntNode> {
-        int value;
-
-        public IntNode(int value) {
-            this.value = value;
-        }
-
-        public int getValue() {
-            return value;
-        }
-
-        @Override
-        public String toString() {
-            return "IntNode{" + value + '}';
-        }
-    }
 }
