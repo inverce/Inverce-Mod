c4e4633f212edce31f3a7e557618a0ddb644a615
Dominik Opolski <dominik.opolski@gmail.com>
2018-04-05 16:48:12 +0200

    small fix,

--- Test/src/main/java/com/inverce/mod/tests/test_utils.kt
+++ Test/src/main/java/com/inverce/mod/tests/test_utils.kt
@@ -1,4 +1,29 @@
 package com.inverce.mod.tests
 
-/** Created on 26/02/2018. */
+import com.inverce.mod.core.IM
+import java.util.concurrent.ScheduledFuture
+import java.util.concurrent.TimeUnit
+
+fun ds() {
+    onBg(delay = 200, command = {
+    }) then onUi({
+
+    }) then onBg({
+
+    })
+
+}
+
+infix fun ScheduledFuture<*>.then(that: ScheduledFuture<*>): ScheduledFuture<*> {
+    return IM.onBg().schedule({
+        get()
+        that.get()
+    }, 0, TimeUnit.MILLISECONDS)
+}
+
+fun onBg(command: () -> Unit, delay: Long = 0, unit: TimeUnit = TimeUnit.MILLISECONDS): ScheduledFuture<*>
+        = IM.onBg().schedule(command, delay, unit)
+
+fun onUi(command: () -> Unit, delay: Long = 0, unit: TimeUnit = TimeUnit.MILLISECONDS): ScheduledFuture<*>
+        = IM.onBg().schedule(command, delay, unit)
 
